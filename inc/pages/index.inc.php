be8c0fd7b8236a24ffe65b4bf7012229c62132cd
freedbrd <vadimtrihleb@gmail.com>
2018-12-19 06:16:30 +0200

    Добавлен контент на главную страницу

--- inc/pages/index.inc.php
+++ inc/pages/index.inc.php
@@ -1,3 +1,58 @@
-<div class="row top_tiles" style="margin: 10px 0;">
-    
+<?php 
+
+	$goods = getAllGoods();
+	$repairs = getGoodsByQuery('status', 1);
+	$shipment = getGoodsByQuery('status', 2);
+	$providers = getAllProviders();
+?>
+<div class="row about top_tiles" style="margin: 10px 0;">
+    <div class="col-md-12 col-sm-12 col-xs-12">
+    	<div class="x-panel">
+    		<h3>Вітаю!</h3>
+    		<p>
+    			Це мій курсовий проект на тему «Склад». <br> Інформацію, про те, як користуватись сайтом знаходиться в розділі 4.4 Опис інтерфейсу програми або в файлі README.md
+    		</p>
+    		<h4>
+    			Невелика статистика по складу:
+    		</h4>
+    		<div class="row top_tiles stat">
+	            <div class="animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-12">
+	              <div class="tile-stats">
+	                <div class="icon"><i class="fa fa-book"></i>
+	                </div>
+	                <div class="count"><?=count($goods)?></div>
+
+	                <h3>Усього позицій на складі</h3>
+	              </div>
+	            </div>
+	            <div class="animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-12">
+	              <div class="tile-stats">
+	                <div class="icon"><i class="fa fa-truck"></i>
+	                </div>
+	                <div class="count"><?=count($providers)?></div>
+
+	                <h3>Постачальників</h3>
+	              </div>
+	            </div>
+	            <div class="animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-12">
+	              <div class="tile-stats">
+	                <div class="icon"><i class="fa fa-wrench"></i>
+	                </div>
+	                <div class="count"><?=count($repairs)?></div>
+
+	                <h3>Товарів в ремонті</h3>
+	              </div>
+	            </div>
+	            <div class="animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-12">
+	              <div class="tile-stats">
+	                <div class="icon"><i class="fa fa-upload"></i>
+	                </div>
+	                <div class="count"><?=count($shipment)?></div>
+
+	                <h3>Товарів на відгрузку</h3>
+	              </div>
+	            </div>
+	          </div>
+    	</div>
+    </div>
 </div>
--- inc/sidebar.php
+++ inc/sidebar.php
@@ -7,18 +7,6 @@
     <div class="clearfix"></div>
 
 
-    <!-- menu prile quick info -->
-    <div class="profile">
-      <div class="profile_pic">
-        <img src="../assets/images/img.jpg" alt="..." class="img-circle profile_img">
-      </div>
-      <div class="profile_info">
-        <span>Вітаємо,</span>
-        <h2>Вадим</h2>
-      </div>
-    </div>
-    <!-- /menu prile quick info -->
-
     <br />
 
     <!-- sidebar menu -->
@@ -80,23 +68,7 @@
       </div>
 
       <ul class="nav navbar-nav navbar-right">
-        <li class="">
-          <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-            <img src="../assets/images/img.jpg" alt="">Вадим
-            <span class=" fa fa-angle-down"></span>
-          </a>
-          <ul class="dropdown-menu dropdown-usermenu animated fadeInDown pull-right">
-            <li><a href="javascript:;">  Профиль</a>
-            </li>
-            <li>
-              <a href="javascript:;">
-                <span>Налаштування</span>
-              </a>
-            </li>
-            <li><a href="login.html"><i class="fa fa-sign-out pull-right"></i> Вийти</a>
-            </li>
-          </ul>
-        </li>
+        
 
       </ul>
     </nav>
